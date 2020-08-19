e79f864db20f05cde9339f9ee667840ceaa7e43c
Maksim Kuntsevich <max-cun4@yandex.ru>
2020-08-19 15:55:30 +0300

    Criteria validator fix

--- src/com/kuntsevich/task1/validator/CriteriaValidator.java
+++ src/com/kuntsevich/task1/validator/CriteriaValidator.java
@@ -8,6 +8,9 @@ public class CriteriaValidator {
     private static final String VALIDATION_REGEX = "[A-Z][a-zA-Z]*";
 
     public static boolean isCriteriaValid(Criteria criteria) {
+        if (criteria == null) {
+            return false;
+        }
         String groupSearchName = criteria.getGroupSearchName();
         return Pattern.matches(VALIDATION_REGEX, groupSearchName);
     }
